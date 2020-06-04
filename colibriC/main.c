16ebc6fdc0bda6a9b135f7cbd37feb76cb114928
Mariana De la Vega <0206627@up.edu.mx>
2020-06-03 19:02:10 -0500

    Initial Commit

---
+++ colibriC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  colibriC
+//
+//  Created by Mariana De la Vega on 6/3/20.
+//  Copyright © 2020 Mariana De la Vega. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
